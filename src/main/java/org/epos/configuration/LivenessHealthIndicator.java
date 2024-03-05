87c3c74e42c2919eedaa3849e5e22ab602ff099f
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2024-03-05 15:58:56 +0100

    check update

--- src/main/java/org/epos/configuration/LivenessHealthIndicator.java
+++ src/main/java/org/epos/configuration/LivenessHealthIndicator.java
@@ -1,5 +1,10 @@
 package org.epos.configuration;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.epos.edmmapping.DBService;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -7,16 +12,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class LivenessHealthIndicator implements HealthIndicator {
 
-    @Override
-    public Health health() {
-        int errorCode = check();
-        if (errorCode != 0) {
-            return Health.down().build();
-        }
-        return Health.up().build();
-    }
+	 @Override
+	    public Health health() {
+	        int errorCode = check();
+	        if (errorCode != 0) {
+	            return Health.down().withDetail("No Database Connection", errorCode).build();
+	        }
+	      
+	        return Health.up().build();
+	    }
 
-    private int check() {
-        return 0;
-    }
+	    private int check() {
+
+	        try (
+	                Connection c = DBService.getDBConnection();
+	                Statement stmt = c.createStatement();
+	                ResultSet resultSetPropertyMapping = stmt.executeQuery("select * from class_mapping cm");
+	        ) {
+	        } catch (Exception ignored){
+	            return 1;
+	        }
+	        return 0;
+	    }
 }
